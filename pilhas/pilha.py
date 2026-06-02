37aececebba9a4d1aebaf8bbaf0b568a11a79ca5
wlzero7 <williamjcampos6@gmail.com>
2026-06-02 17:29:04 -0300

    Add basic stack implementation in pilha.py

---
+++ pilhas/pilha.py
@@ -0,0 +1,15 @@
+# pilhas/pilha.py
+
+pilha = []
+
+pilha.append("Livro 1")
+pilha.append("Livro 2")
+pilha.append("Livro 3")
+
+print("Pilha atual:")
+print(pilha)
+
+removido = pilha.pop()
+
+print(f"Removido: {removido}")
+print(pilha)
