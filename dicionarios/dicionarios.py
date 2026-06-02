47a6d93a27a75aa3aeec457eb520dd58532939b0
wlzero7 <williamjcampos6@gmail.com>
2026-06-02 17:28:34 -0300

    Add aluno dictionary with print statements

---
+++ dicionarios/dicionarios.py
@@ -0,0 +1,10 @@
+# dicionarios/dicionarios.py
+
+aluno = {
+    "nome": "William",
+    "idade": 18,
+    "curso": "Ciência da Computação"
+}
+
+print(f"Nome: {aluno['nome']}")
+print(f"Curso: {aluno['curso']}")
